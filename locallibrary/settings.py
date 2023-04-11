28fb2abc9d86ce9658a027334fadf7566ea1abd2
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2023-04-11 23:47:36 +0100

    added secret information in env variables

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%o@_jqrap2@%#uojy8@gmt&rdurh=9obg4j=bm3)e+(s1-kfk@'
+SECRET_KEY = os.getenv("SECRET_KEY", 'django-insecure-%o@_jqrap2@%#uojy8@gmt&rdurh=9obg4j=bm3)e+(s1-kfk@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", True)
 
 ALLOWED_HOSTS = []
 
