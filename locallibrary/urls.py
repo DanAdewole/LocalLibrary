24fccbb9bb9d15b37b9a6a399c3bc02740ad5467
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2023-04-21 13:05:10 +0100

    Remove reset password functionality
    Twilio sendgrid not working well (domain authentication problems)

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -19,9 +19,12 @@ from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
+# from catalog.views import CustomPasswordResetView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
