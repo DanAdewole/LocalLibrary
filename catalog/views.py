9bcfacae6dfbb370a29d749e87ae4fa433ea5ec8
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2023-04-21 13:04:07 +0100

    Reformatted the views with black

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
@@ -13,7 +14,21 @@ from django.contrib.auth import authenticate, login
 from django.db.models import Q
 
 from .models import Book, Author, BookInstance, Genre, Language
-from catalog.forms import RenewBookForm, CreateBookInstanceForm, CustomPasswordResetForm, CustomUserCreationForm
+from catalog.forms import (
+    RenewBookForm,
+    CreateBookInstanceForm,
+    CustomPasswordResetForm,
+    CustomUserCreationForm,
+)
+
+# Environment variables
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# 3rd party packages
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 
 # def index(request):
@@ -69,7 +84,7 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
-    context_object_name = 'book'
+    context_object_name = "book"
     template_name = "book_detail.html"
 
 
@@ -78,25 +93,27 @@ class BookInstanceCreateView(LoginRequiredMixin, CreateView):
     form_class = CreateBookInstanceForm
     # fields = ['imprint', 'due_back', 'status']
     # context_object_name = "bookinstance"
-    template_name = 'bookinstance_form.html'
-    success_url = reverse_lazy('my-borrowed')
-    
+    template_name = "bookinstance_form.html"
+    success_url = reverse_lazy("my-borrowed")
+
     def form_valid(self, form):
         form.instance.borrower = self.request.user
-        form.instance.book = get_object_or_404(Book, id=self.kwargs['book_id'])
-        form.instance.status = 'o'
+        form.instance.book = get_object_or_404(Book, id=self.kwargs["book_id"])
+        form.instance.status = "o"
         return super().form_valid(form)
-    
+
     def get_initial(self):
         initial = super().get_initial()
-        initial['status'] = 'o'  # Set status to "On loan"
-        initial['borrower'] = self.request.user  # Set borrower to current user
-        initial['book'] = get_object_or_404(Book, id=self.kwargs['book_id'])  # Set book to clicked book
+        initial["status"] = "o"  # Set status to "On loan"
+        initial["borrower"] = self.request.user  # Set borrower to current user
+        initial["book"] = get_object_or_404(
+            Book, id=self.kwargs["book_id"]
+        )  # Set book to clicked book
         return initial
-    
+
     def get_context_data(self, **kwargs):
         context = super(BookInstanceCreateView, self).get_context_data(**kwargs)
-        context["book"] = Book.objects.get(pk=self.kwargs['book_id'])
+        context["book"] = Book.objects.get(pk=self.kwargs["book_id"])
         context["form"] = self.get_form()
         return context
 
@@ -130,17 +147,19 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
         )
 
 
-class LoanedBooksAllListView(LoginRequiredMixin, PermissionRequiredMixin, generic.ListView):
+class LoanedBooksAllListView(
+    LoginRequiredMixin, PermissionRequiredMixin, generic.ListView
+):
     """Generic class-based view listing all books on loan."""
 
     model = BookInstance
     template_name = "bookinstance_list_all.html"
     context_object_name = "bookinstance_list_all"
     paginate_by = 10
-    permission_required = ("catalog.can_mark_returned")
+    permission_required = "catalog.can_mark_returned"
 
     def get_queryset(self):
-        return (BookInstance.objects.filter(status__exact="o").order_by("due_back"))
+        return BookInstance.objects.filter(status__exact="o").order_by("due_back")
 
 
 # Get all books with a particular genre
@@ -148,17 +167,18 @@ class BookGenreListview(generic.ListView):
     template_name = "book_list_by_genre.html"
     context_object_name = "books"
     paginate_by = 5
-    
+
     def get_queryset(self):
         genre = self.kwargs["genre"]
-        queryset  = Book.objects.filter(genre__name__in=[genre])
+        queryset = Book.objects.filter(genre__name__in=[genre])
         return queryset
-    
+
     def get_context_data(self, **kwargs):
         context = super(BookGenreListview, self).get_context_data(**kwargs)
         context["genre"] = self.kwargs["genre"]
         context["all_genre"] = Genre.objects.all()
         return context
+
 
 # as a function based view
 # def book_detail_view(request, primary_key):
@@ -171,138 +191,169 @@ class BookGenreListview(generic.ListView):
 
 
 @login_required
-@permission_required('catalog.can_mark_returned', raise_exception=True)
+@permission_required("catalog.can_mark_returned", raise_exception=True)
 def renew_book_librarian(request, pk):
     """View function for renewing a specific BookInstance by librarian."""
     book_instance = get_object_or_404(BookInstance, pk=pk)
 
     # If this is a POST request then process the Form data
-    if request.method == 'POST':
-
+    if request.method == "POST":
         # Create a form instance and populate it with data from the request (binding):
         form = RenewBookForm(request.POST)
 
         # Check if the form is valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
-            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.due_back = form.cleaned_data["renewal_date"]
             book_instance.save()
 
             # redirect to a new URL:
-            return HttpResponseRedirect(reverse('all-borrowed'))
+            return HttpResponseRedirect(reverse("all-borrowed"))
 
     # If this is a GET (or any other method) create the default form.
     else:
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+        form = RenewBookForm(initial={"renewal_date": proposed_renewal_date})
 
     context = {
-        'form': form,
-        'book_instance': book_instance,
+        "form": form,
+        "book_instance": book_instance,
     }
 
-    return render(request, 'book_renew_librarian.html', context)
+    return render(request, "book_renew_librarian.html", context)
 
 
 class AuthorCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Author
-    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
-    initial = {'date_of_death': '05/01/2023'}
-    permission_required = ('catalog.add_author')
-    template_name = 'author_form.html'
-    
+    fields = ["first_name", "last_name", "date_of_birth", "date_of_death"]
+    initial = {"date_of_death": "05/01/2023"}
+    permission_required = "catalog.add_author"
+    template_name = "author_form.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['creating'] = True
+        context["creating"] = True
         return context
-    
+
 
 class AuthorUpdate(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Author
-    fields = '__all__'  # Not recommended (potential security issue if more fields added)
-    permission_required = ('catalog.change_author')
-    template_name = 'author_form.html'
-    
+    fields = (
+        "__all__"  # Not recommended (potential security issue if more fields added)
+    )
+    permission_required = "catalog.change_author"
+    template_name = "author_form.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['creating'] = False
+        context["creating"] = False
         return context
-    
-    
+
+
 class AuthorDelete(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Author
-    success_url = reverse_lazy('authors')
-    permission_required = ('catalog.delete_author')
+    success_url = reverse_lazy("authors")
+    permission_required = "catalog.delete_author"
     template_name = "author_confirm_delete.html"
 
 
 class BookCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Book
-    fields = ['title', 'author', 'summary', 'isbn', 'genre', 'language']
-    permission_required = ('catalog.add_book')
+    fields = ["title", "author", "summary", "isbn", "genre", "language"]
+    permission_required = "catalog.add_book"
     template_name = "book_form.html"
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['creating'] = True
+        context["creating"] = True
         return context
-    
+
 
 class BookUpdate(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Book
-    fields = '__all__'  # Not recommended (potential security issue if more fields added)
-    permission_required = ('catalog.change_book')
+    fields = (
+        "__all__"  # Not recommended (potential security issue if more fields added)
+    )
+    permission_required = "catalog.change_book"
     template_name = "book_form.html"
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['creating'] = False
+        context["creating"] = False
         return context
-    
+
 
 class BookDelete(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Book
-    success_url = reverse_lazy('index')
-    permission_required = ('catalog.delete_book')
+    success_url = reverse_lazy("index")
+    permission_required = "catalog.delete_book"
     template_name = "book_confirm_delete.html"
-    
 
-class CustomPasswordResetView(PasswordResetView):
-    form_class = CustomPasswordResetForm
-    template_name = 'password_reset.html'
+
+# class CustomPasswordResetView(PasswordResetView):
+#     form_class = CustomPasswordResetForm
+#     template_name = "password_reset.html"
+#     success_url = reverse_lazy("password_reset_done")
+
+#     def form_valid(self, form):
+#         response = super().form_valid(form)
+
+#         message = Mail(
+#             from_email="daniel@tedprimehub.org",
+#             to_emails=form.cleaned_data["email"],
+#             subject="Reset your password",
+#             html_content=self.render_to_string(
+#                 self.template_name,
+#                 {"user": form.user, "reset-url": self.get_reset_url(form)},
+#             ),
+#         )
+#         try:
+#             SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+#             sg = SendGridAPIClient(SENDGRID_API_KEY)
+#             response = sg.send(message)
+#             print(response.status_code)
+#             print(response.body)
+#             print(response.headers)
+#         except Exception as e:
+#             print(e)
+
+#         return response
 
 
 def signup(request):
-    if request.method == 'POST':
-        form  = CustomUserCreationForm(request.POST)
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password = password)
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=password)
             login(request, user)
-            return redirect('index')
+            return redirect("index")
     else:
         form = CustomUserCreationForm()
-    return render(request, 'registration/signup.html', {'form': form})
-    
-    
+    return render(request, "registration/signup.html", {"form": form})
+
+
 class SearchResultsView(generic.ListView):
     model = Book
-    template_name = 'book_search.html'
-    context_object_name = 'book_search'
-    
+    template_name = "book_search.html"
+    context_object_name = "book_search"
+
     def get_queryset(self):
         query = self.request.GET.get("q")
         if query:
             # Search for books that match the query in title or author's name
             return Book.objects.filter(
-                Q(title__icontains=query) | Q(author__first_name__icontains=query) | Q(summary__icontains=query) | Q(author__last_name__icontains=query)
+                Q(title__icontains=query)
+                | Q(author__first_name__icontains=query)
+                | Q(summary__icontains=query)
+                | Q(author__last_name__icontains=query)
                 # Q(title__icontains=query) | Q(summary__icontains=query)
             )
         else:
             return Book.objects.none()
-        
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         query = self.request.GET.get("q")
